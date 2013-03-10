b4a7f6d33deeeda050f1b99a302ed7aebb700f93
Àlex Marlés <newyorkdc@gmail.com>
2013-03-10 01:03:44 +0100

    Added draw function

--- javascript/classification.js
+++ javascript/classification.js
@@ -1,5 +1,7 @@
-// Creating canvas
-var canvas = document.getElementById("game");
-var ctx = canvas.getContext("2d");
-canvas.width = 400;
-canvas height = 400;
+function draw() {
+  // Creating canvas
+  var canvas = document.getElementById("game");
+  var ctx = canvas.getContext("2d");
+  canvas.width = 400;
+  canvas.height = 400;
+}
