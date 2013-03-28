f2c7e931721ec4d15cecf9cfd4740c93c1e3bf40
Àlex Marlés <newyorkdc@gmail.com>
2013-03-28 12:44:18 +0100

    Added multiple items and handling collisions

--- javascript/classification.js
+++ javascript/classification.js
@@ -3,17 +3,12 @@ function draw() {
   // Creating canvas
   var canvas = document.getElementById("game");
   var ctx = canvas.getContext("2d");
-  // canvas.width = 360;
-  // canvas.height = 640;
-  canvas.width = window.innerWidth/2;
-  canvas.height = window.innerHeight/2;
+  canvas.width = 360;
+  canvas.height = 640;
 
-  // Constants
-  var kbLeft = 37;
-  var kbUp = 38;
-  var kbRight = 39;
-  var kbDown = 40;
-
+  // ------------------------------
+  // CONSTANTS
+  // ------------------------------
   var itemSpeed = 150;
   var itemWidth = 40;
   var itemHeight = 40;
@@ -21,7 +16,9 @@ function draw() {
   var containerWidth = 50;
   var containerHeight = 75;
 
-  // Objects
+  // ------------------------------
+  // OBJECTS
+  // ------------------------------
   var pointer = {
     x: 0,
     y: 0,
@@ -29,242 +26,243 @@ function draw() {
     height: 1
   };
 
-  var item = {
+  var item0 = {
     speed: itemSpeed,
     type: 0,
     x: 0,
     y: 0,
     width: itemWidth,
-    height: itemHeight
+    height: itemHeight,
+    active: true
   };
+
+  var item1 = {
+    speed: itemSpeed,
+    type: 0,
+    x: 0,
+    y: 0,
+    width: itemWidth,
+    height: itemHeight,
+    active: true
+  };
+
+  var item2 = {
+    speed: itemSpeed,
+    type: 0,
+    x: 0,
+    y: 0,
+    width: itemWidth,
+    height: itemHeight,
+    active: true
+  };
+
+  var item3 = {
+    speed: itemSpeed,
+    type: 0,
+    x: 0,
+    y: 0,
+    width: itemWidth,
+    height: itemHeight,
+    active: true
+  };
+
+  var item4 = {
+    speed: itemSpeed,
+    type: 0,
+    x: 0,
+    y: 0,
+    width: itemWidth,
+    height: itemHeight,
+    active: true
+  };
+
+  var items = [item0, item1, item2, item3, item4];
+  var itemsToMove = [];
 
   var containerRed = {
     type: 0,
     x: 0,
-    y: 0
+    y: 0,
+    width: containerWidth,
+    height: containerHeight,
+    score: 0
   };
 
   var containerBlue = {
     type: 1,
     x: 0,
-    y: 0
+    y: 0,
+    width: containerWidth,
+    height: containerHeight,
+    score: 0
   };
 
   var containerGreen = {
     type: 2,
     x: 0,
-    y: 0
+    y: 0,
+    width: containerWidth,
+    height: containerHeight,
+    score: 0
   };
+  
+  var containers = [containerRed, containerGreen, containerBlue];
 
-  var itemsRed = 0;
-  var itemsBlue = 0;
-  var itemsGreen = 0;
-  var items = 0;
+  // ------------------------------
+  // VARIABLES
+  // ------------------------------
 
-  // Check collision
-  var collided = false;
+  var totalScore = 0;
+  var pointerActive = false;
+  var picked = false;
 
-  var checkCollision = function (one, two) {
-    if(two.x >= one.x && two.x <= (one.x + one.width)) {
-      if(two.y >= one.y && two.y <= (one.y + one.height)) {
-        collided = true;
-      }
-    }
-  };
-
-  // Keyboard controls
-  var keysDown = {};
-
-  addEventListener("keydown", function (e) {
-    keysDown[e.keyCode] = true;
-  }, false);
-
-  addEventListener("keyup", function (e) {
-    delete keysDown[e.keyCode];
-  }, false);
+  // ------------------------------
+  // CONTROLS
+  // ------------------------------
 
   // Mouse controls
-  var mouseDown = false;
-  var mousePos;
-  
   var getMousePos = function (e) {
     var rect = canvas.getBoundingClientRect();
     return {
       x: e.clientX - rect.left,
-      y: e.clientY - rect.top
+      y: e.clientY - rect.top,
+      width: 1,
+      height: 1
     };
   };
 
   addEventListener("mousedown", function (e) {
-    mouseDown = true;
-    mousePos = getMousePos(e);
-    pointer.x = mousePos.x;
-    pointer.y = mousePos.y;
-    checkCollision(item, pointer);
-    console.log("MOUSE DOWN");
-    console.log("Mouse position: " + mousePos.x + ", " + mousePos.y);
+    pointerActive = true;
+    pointer = getMousePos(e);
   }, false);
 
   addEventListener("mouseup", function (e) {
-    mouseDown = false;
-    collided = false;
-    console.log("MOUSE UP");
+    pointerActive = false;
+    picked = false;
+    itemsToMove = [];
   }, false);
   
   addEventListener("mousemove", function (e) {
-    if(mouseDown) {
-      mousePos = getMousePos(e);
-      console.log("Mouse position: " + mousePos.x + ", " + mousePos.y);
-      if(collided) {
-        item.x = mousePos.x;
-        item.y = mousePos.y;
-      }
+    if (pointerActive) {
+      pointer = getMousePos(e);
+      itemsToMove.forEach(function(iTM) {
+        iTM.x = pointer.x;
+        iTM.y = pointer.y;
+      });
     }
   }, false);
 
   // Touch controls
-  var touched = false;
-  var touchPos;
-
   var getTouchPos = function (e) {
     return {
       x: e.targetTouches[0].pageX,
-      y: e.targetTouches[0].pageY
+      y: e.targetTouches[0].pageY,
+      width: 1,
+      height: 1
     };
   };
 
   addEventListener("touchstart", function (e) {
-    touched = true;
-    touchPos = getTouchPos(e);
-    pointer.x = touchPos.x;
-    pointer.y = touchPos.y;
-    checkCollision(item, pointer);
-    console.log("TOUCHED");
-    console.log("Touched in " + touchPos.x + ", " + touchPos.y);
+    pointerActive = true;
+    pointer = getTouchPos(e);
   }, false);
 
   addEventListener("touchend", function (e) {
-    touched = false;
-    collided = false;
-    console.log("TOUCH UP");
+    pointerActive = false;
+    picked = false;
+    itemsToMove = [];
   }, false);
 
   addEventListener("touchmove", function (e) {
-    if(touched) {
-      touchPos = getTouchPos(e);
-      console.log("Touched in " + touchPos.x + ", " + touchPos.y);
-      if(collided) {
-        item.x = touchPos.x;
-        item.y = touchPos.y;
-      }
+    if (pointerActive) {
+      pointer = getTouchPos(e);
+      itemsToMove.forEach(function(iTM) {
+        iTM.x = pointer.x;
+        iTM.y = pointer.y;
+      });
     }
   }, false);
 
+  // ------------------------------
+  // FUNCTIONS
+  // ------------------------------
 
-  // Reset the game when item is in container line
-  var reset = function () {
-    item.x = canvas.width / 2;
-    item.y = 20;
-    item.type = parseInt(Math.random() * 3);
+  // Check collision
+  var collides = function (a, b) {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+  };
 
-    containerRed.x = (canvas.width/6)*1 - (containerWidth/2);
-    containerRed.y = canvas.height - (containerHeight + 10);
-    containerBlue.x = (canvas.width/6)*3 - (containerWidth/2);
-    containerBlue.y = canvas.height - (containerHeight + 10);
-    containerGreen.x = (canvas.width/6)*5 - (containerWidth/2);
-    containerGreen.y = canvas.height - (containerHeight + 10);
+  var handleCollisions = function () {
+    if (pointerActive) {
+      items.forEach(function(item) {
+        if (collides(item, pointer) && !picked) {
+          itemsToMove.push(item);
+          picked = true;
+        }
+      });
+    }
+
+    items.forEach(function(item) {
+      containers.forEach(function(container) {
+        if (collides(item, container)) {
+          if (item.type === container.type) {
+            ++container.score;
+            item.active = false;
+          }
+        }
+      });
+    });
   };
 
   // Update game objects
   var update = function (modifier) {
-    item.y += 0.3;
-
-    if(kbDown in keysDown) {
-      if(item.y+itemHeight <= canvas.height) {
-        item.y += item.speed*modifier;
-      }
-    }
-    if(kbLeft in keysDown) {
-      if(item.x > 0) {
-        item.x -= item.speed*modifier;
-      }
-    }
-    if(kbRight in keysDown) {
-      if(item.x+itemWidth <= canvas.width) {
-        item.x += item.speed*modifier;
-      }
-    }
-
-    if((item.y+itemHeight) >= (canvas.height-containerHeight/2)) {
-      if((item.x >= containerRed.x
-          && item.x <= (containerRed.x+containerWidth)
-          && item.type == containerRed.type)
-          ||
-          ((item.x+itemWidth) >= containerRed.x
-           && (item.x+itemWidth) <= (containerRed.x+containerWidth)
-           && item.type == containerRed.type)
-        ) {
-          ++itemsRed;
-      }
-      if((item.x >= containerBlue.x
-          && item.x <= (containerBlue.x+containerWidth)
-          && item.type === containerBlue.type)
-          ||
-          ((item.x+itemWidth) >= containerBlue.x
-           && (item.x+itemWidth) <= (containerBlue.x+containerWidth)
-           && item.type == containerBlue.type)
-        ) {
-          ++itemsBlue;
-      }
-      if((item.x >= containerGreen.x
-          && item.x <= (containerGreen.x+containerWidth)
-          && item.type === containerGreen.type)
-          ||
-          ((item.x+itemWidth) >= containerGreen.x
-           && (item.x+itemWidth) <= (containerGreen.x+containerWidth)
-           && item.type == containerGreen.type)
-        ) {
-          ++itemsGreen;
-      }
-      reset();
-      collided = false;
-    }
+    handleCollisions();
+    items.forEach( function(item) {
+      item.y += 0.1;
+      if((item.y+itemHeight) >= (canvas.height-containerHeight/2) || !item.active) {
+        item.x = parseInt(Math.random() * (canvas.width - 20) + 10);
+        item.y = 20;
+        item.type = parseInt(Math.random() * 3);
+        item.active = true;
+        pointerActive = false;
+      };
+    });
   };
  
   var render = function () {
-    // ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    if(item.type == 0) {
-      ctx.fillStyle = "red";
-    } else if(item.type == 1) {
-      ctx.fillStyle = "blue";
-    } else if(item.type == 2) {
-      ctx.fillStyle = "green";
-    }
-    ctx.fillRect(item.x, item.y, itemWidth, itemHeight);
+    items.forEach( function(item) {
+      if(item.type === 0) {
+        ctx.fillStyle = "red";
+      } else if(item.type === 1) {
+        ctx.fillStyle = "blue";
+      } else if(item.type === 2) {
+        ctx.fillStyle = "green";
+      }
+      ctx.fillRect(item.x, item.y, itemWidth, itemHeight);
+    });
 
     ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-    ctx.fillRect(containerRed.x, containerRed.y, containerWidth, containerHeight);
+    ctx.fillRect(containerRed.x, containerRed.y, containerRed.width, containerRed.height);
     ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
-    ctx.fillRect(containerBlue.x, containerBlue.y, containerWidth, containerHeight);
+    ctx.fillRect(containerBlue.x, containerBlue.y, containerBlue.width, containerBlue.height);
     ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
-    ctx.fillRect(containerGreen.x, containerGreen.y, containerWidth, containerHeight);
+    ctx.fillRect(containerGreen.x, containerGreen.y, containerGreen.width, containerGreen.height);
 
     ctx.font = "16px Helvetica";
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
     ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
-    ctx.fillText("Red: " + itemsRed, 5, 5);
+    ctx.fillText("Red: " + containerRed.score, 5, 5);
     ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
-    ctx.fillText("Blue: " + itemsBlue, 5, 20);
+    ctx.fillText("Blue: " + containerBlue.score, 5, 20);
     ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
-    ctx.fillText("Green: " + itemsGreen, 5, 35);
-
-    ctx.font = "12px Helvetica";
-    ctx.fillText("Pos item: (" + parseInt(item.x) + ", " + parseInt(item.y) +" )", 100, 5);
-    ctx.fillText("Type item: " + parseInt(item.type), 100, 15);
+    ctx.fillText("Green: " + containerGreen.score, 5, 35);
   };
 
   var main = function () {
@@ -277,7 +275,17 @@ function draw() {
     then = now;
   };
 
-  reset();
+  items.forEach( function(item) {
+    item.x = parseInt(Math.random() * (canvas.width - 20) + 10);
+    item.y = 20;
+    item.type = parseInt(Math.random() * 3);
+  });
+  containerRed.x = (canvas.width/6)*1 - (containerWidth/2);
+  containerRed.y = canvas.height - (containerHeight + 10);
+  containerBlue.x = (canvas.width/6)*3 - (containerWidth/2);
+  containerBlue.y = canvas.height - (containerHeight + 10);
+  containerGreen.x = (canvas.width/6)*5 - (containerWidth/2);
+  containerGreen.y = canvas.height - (containerHeight + 10);
   var then = Date.now();
   setInterval(main, 1);
 }
